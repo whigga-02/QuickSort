0fa9510901be541188c55401523a2b3a820fe424
whigga_02 <jodsonjod@gmail.com>
2020-02-25 20:12:31 +0200

    anuva commit - anuva code

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -4,49 +4,66 @@ namespace QuickSort
 {
     class Program
     {
+        //метод для обмена элементов массива
+        static void Swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+
+        //метод возвращающий индекс опорного элемента
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+
+        //быстрая сортировка
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+
+        static int[] QuickSort(int[] array)
+        {
+            return QuickSort(array, 0, array.Length - 1);
+        }
+
         static void Main(string[] args)
         {
-            
+            Console.Write("N = ");
+            var len = Convert.ToInt32(Console.ReadLine());
+            var a = new int[len];
+            for (var i = 0; i < a.Length; ++i)
+            {
+                Console.Write("a[{0}] = ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Упорядоченный массив: {0}", string.Join(", ", QuickSort(a)));
+
+            Console.ReadLine();
         }
-        public static void Quicksort(IComparable[] elements, int left, int right)
-        {
-            int i = left, j = right;
-            IComparable pivot = elements[(left   right) / 2];
-            while (i <= j)
-            {
-                while (elements[i].CompareTo(pivot) < 0)
-                {
-                    i  ;
-                }
-
-                while (elements[j].CompareTo(pivot) > 0)
-                {
-                    j--;
-                }
-
-                if (i <= j)
-                {
-                    // Swap
-                    IComparable tmp = elements[i];
-                    elements[i] = elements[j];
-                    elements[j] = tmp;
-
-                    i  ;
-                    j--;
-                }
-            }
-
-            // Recursive calls
-            if (left < j)
-            {
-                Quicksort(elements, left, j);
-            }
-
-            if (i < right)
-            {
-                Quicksort(elements, i, right);
-            }
-        }
-
     }
 }
